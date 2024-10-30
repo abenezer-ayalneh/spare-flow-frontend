186f80a4cceb2aae80b0f643c51f54885d3ad816
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2024-10-30 09:27:59 +0300

    chore: first commit

---
+++ src/app/layouts/full/vertical/sidebar/sidebar-data.ts
@@ -0,0 +1,46 @@
+import { NavItem } from './nav-item/nav-item';
+
+export const navItems: NavItem[] = [
+  {
+    navCap: 'Home',
+  },
+  {
+    displayName: 'Starter',
+    iconName: 'home',
+    route: '/starter',
+  },
+  {
+    navCap: 'Other',
+  },
+  {
+    displayName: 'Menu Level',
+    iconName: 'box-multiple',
+    route: '/menu-level',
+    children: [
+      {
+        displayName: 'Menu 1',
+        iconName: 'point',
+        route: '/menu-1',
+        children: [
+          {
+            displayName: 'Menu 1',
+            iconName: 'point',
+            route: '/menu-1',
+          },
+
+          {
+            displayName: 'Menu 2',
+            iconName: 'point',
+            route: '/menu-2',
+          },
+        ],
+      },
+
+      {
+        displayName: 'Menu 2',
+        iconName: 'point',
+        route: '/menu-2',
+      },
+    ],
+  },
+];
